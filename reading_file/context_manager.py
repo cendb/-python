052cea4d30e37a7b51ac0001403ec3c90f7819d1
Jhon Vincent Sosa <jhonvincentp.sosa@gmail.com>
2026-08-11 21:22:58 +0800

    commit

---
+++ reading_file/context_manager.py
@@ -0,0 +1,6 @@
+filename = 'sample_data.txt'
+
+with open(filename, mode='r') as file:
+    content = file.read()
+    print(content)
+
---
+++ reading_file/text.py
@@ -0,0 +1,5 @@
+file_name = 'sample_data.txt'
+
+file = open(file_name, mode='r')
+content = file.read()
+file.close()
